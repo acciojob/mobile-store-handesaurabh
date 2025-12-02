e707423402a52e9105ea995aac28d3e61286714c
handesaurabh <handesaurabh2004@gmail.com>
2025-12-02 15:21:17 +0530

    Create adminProduct.js

---
+++ src/components/adminProduct.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const AdminProdcut = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <p>Product {id}</p>
+      <input name="title" id="title" />
+
+      <button>Delete</button>
+      <button>Save</button>
+    </div>
+  );
+};
+
+export default AdminProdcut;
